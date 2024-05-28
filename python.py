245a3da3293e65d71fb3e2c2cc8683c8dbf96e66
elhafdiimane717@icloud.com <elhafdiimane717@icloud.com>
2024-05-28 16:43:39 +0100

    Add Django backend

---
+++ python.py
@@ -0,0 +1,16 @@
+INSTALLED_APPS = [
+    
+    'store',
+] 
+
+#defining models
+from django.db import models 
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
